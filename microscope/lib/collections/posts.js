e4163284b2744e46f279d4398ab83052c1959012
My Högblom <E600574@GOT-MYHOG.corp.corpcommon.com>
2015-04-09 09:34:13 +0200

    Denied access to submit post page when not logged in

--- microscope/lib/collections/posts.js
+++ microscope/lib/collections/posts.js
@@ -3,6 +3,6 @@ Posts = new Mongo.Collection('posts');
 Posts.allow({
   insert: function(userId, doc) {
     //Only allow posting if you are logged in
-    return !! userId
+    return !! userId;
   }
 });
