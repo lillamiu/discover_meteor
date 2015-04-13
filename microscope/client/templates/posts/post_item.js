e09f542aac9b7828fa4d40a6965430aa4d6bea3f
My Högblom <E600574@GOT-MYHOG.corp.corpcommon.com>
2015-04-13 16:45:45 +0200

    display comments on postPage

--- microscope/client/templates/posts/post_item.js
+++ microscope/client/templates/posts/post_item.js
@@ -2,9 +2,12 @@ Template.postItem.helpers({
   ownPost: function() {
     return this.userId === Meteor.userId();
   },
-	domain : function() {
+	domain: function() {
 		var a = document.createElement('a');
 		a.href = this.url;
 		return a.hostname;
-	}
+	},
+  commentsCount: function() {
+    return Comments.find({postId: this._id}).count();
+  }
 });
